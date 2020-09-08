34f4a2ecec05882427444dd3bf0f1f2ea6249746
Unknown <maimon.rose@gmail.com>
2020-09-08 07:29:37 -0700

    added remote copying for batch processing of lfp files

--- batch_extract_lfp_data.m
+++ batch_extract_lfp_data.m
@@ -1,6 +1,11 @@
-function [success,errs] = batch_extract_lfp_data(eData,baseDir)
+function [success,errs] = batch_extract_lfp_data(eData,baseDir,varargin)
 
-overwrite_flag = false;
+pnames = {'localDir','overwrite_flag'};
+dflts  = {[],false,false};
+[localDir,overwrite_flag] = internal.stats.parseArgs(pnames,dflts,varargin{:});
+
+remote_copy_flag = ~isempty(localDir);
+
 total_dirs = 1;
 t = tic;
 success = [];
@@ -13,9 +18,44 @@ end
 
 lastProgress = 0;
 for k = 1:length(exp_dirs)
-    exp_dir = fullfile(exp_dirs(k).folder,exp_dirs(k).name);
+    [exp_dir,remote_exp_dir] = deal(fullfile(exp_dirs(k).folder,exp_dirs(k).name));
+    if remote_copy_flag
+        exp_date_str = exp_dirs(k).name;
+        expDate = datetime(exp_date_str,'InputFormat','MMddyyyy');
+        local_exp_dir = fullfile(localDir,exp_date_str);
+        exp_dir = local_exp_dir;
+    end
     for b = 1:length(eData.batNums)
-        if ~isempty(dir(fullfile(exp_dir,'neurologgers','**',[eData.batNums{b} '*CSC0.mat'])))
+        nlgDir = dir(fullfile(remote_exp_dir,'neurologgers','**',[eData.batNums{b} '*CSC0.mat']));
+        if ~isempty(dir(fullfile(remote_exp_dir,'neurologgers','**',[eData.batNums{b} '*CSC0.mat'])))
+            if remote_copy_flag
+                remote_nlg_dir = fullfile(nlgDir(1).folder,'*CSC*.mat');
+                remote_lfp_dir = fullfile(remote_exp_dir,'lfpformat');
+                
+                if ~overwrite_flag &&  ~isempty(dir(fullfile(remote_lfp_dir,[eData.batNums{b} '*LFP.mat'])))
+                    disp('LFP file already exists')
+                    continue
+                end
+                
+                T = get_rec_logs;
+                
+                T = T(T.Date == expDate,:);
+                sessType = T.Session{find(ismember(T.Session,{'social','vocal'}),1,'first')};
+                remote_audio_dir = fullfile(remote_exp_dir,'audio',sessType,'ch1','audio2nlg_fit.mat');
+                
+                local_nlg_dir = fullfile(local_exp_dir,'neurologgers','extracted_data');
+                local_audio_dir = fullfile(local_exp_dir,'audio',sessType,'ch1');
+                mkdir(local_audio_dir)
+                
+                if ~exist(remote_audio_dir,'file')
+                    disp('Could not file audio2nlg file')
+                    errs{total_dirs} = 'Could not file audio2nlg file';
+                    continue
+                end
+                remote_copy(remote_audio_dir,local_audio_dir);
+                remote_copy(remote_nlg_dir,local_nlg_dir);
+            end
+            
             try
                 extract_lfp_data(exp_dir,eData.expType{b},eData.batNums{b},overwrite_flag,eData.activeChannels{b});
             catch err
@@ -23,6 +63,18 @@ for k = 1:length(exp_dirs)
                 success(total_dirs) = false;
             end
             success(total_dirs) = true;
+            
+            if remote_copy_flag
+                local_lfp_dir = fullfile(local_exp_dir,'lfpformat');
+                remote_lfp_dir = fullfile(remote_exp_dir,'lfpformat');
+                remote_copy(local_lfp_dir,remote_lfp_dir);
+                
+                [rmdir_status, rmdir_err_msg] = rmdir(local_exp_dir,'s');
+                if ~rmdir_status
+                    disp(rmdir_err_msg)
+                    keyboard
+                end
+            end
             
             progress = 100*(total_dirs/length(all_exp_dirs));
             elapsed_time = round(toc(t));
@@ -35,4 +87,26 @@ for k = 1:length(exp_dirs)
             lastProgress = progress;
         end
     end
+end
+
+end
+
+function [status,copy_err_msg] = remote_copy(remoteDir,localDir)
+
+status = false;
+copy_err_msg = [];
+
+TicTransfer = tic;
+
+while ~status && toc(TicTransfer)<30*60
+    [status,copy_err_msg] = copyfile(remoteDir,localDir,'f');
+end
+
+
+if ~status
+    disp('Failed to copy folder TO server after 30 minutes')
+    disp(copy_err_msg)
+    keyboard
+end
+
 end
